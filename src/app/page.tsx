216f9b5190ecd4e005997087b4c9ed599aebdc8d
pheuberger <philipp@pheuberger.com>
2024-10-31 13:34:43 +0200

    feat: introduce AuthBoundary
    Everything wrapped in this tag will require being logged in. When not
    logged in it will show a message asking you to login.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@
 import Footer from "src/components/Footer";
 import Header from "src/components/Header";
 import EligibilityModal from "src/components/EligibilityModal";
+import AuthBoundary from "@/components/AuthBoundary";
 
 export default function Home() {
   return (
     <div className="flex h-full w-96 max-w-full flex-col px-1 md:w-[1008px]">
       <Header />
       <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 px-2 py-4 md:grow">
-        <EligibilityModal />
+        <AuthBoundary>
+          <EligibilityModal />
+        </AuthBoundary>
       </section>
       <Footer />
     </div>
---
+++ src/components/AuthBoundary.tsx
@@ -0,0 +1,26 @@
+import { useAccount } from "wagmi";
+import { ReactNode } from "react";
+
+type AuthBoundaryProps = {
+  children: ReactNode;
+  fallback?: ReactNode;
+};
+
+export default function AuthBoundary({
+  children,
+  fallback,
+}: AuthBoundaryProps) {
+  const { address } = useAccount();
+
+  if (!address) {
+    return (
+      fallback || (
+        <div className="flex w-full flex-col items-center justify-center gap-4">
+          <p>Please login to continue</p>
+        </div>
+      )
+    );
+  }
+
+  return <>{children}</>;
+}
